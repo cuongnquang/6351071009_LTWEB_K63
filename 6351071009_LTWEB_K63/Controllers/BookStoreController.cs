0f8b9c19923681087a0a849d31a519c50cb57539
cuongnquang <cuongnquang@gmail.com>
2024-11-01 19:23:40 +0700

    Up lab1 2 3

---
+++ 6351071009_LTWEB_K63/Controllers/BookStoreController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _6351071009_LTWEB_K63.Models;
+
+namespace _6351071009_LTWEB_K63.Controllers
+{
+    public class BookStoreController : Controller
+    {
+        // GET: BookStore
+        QLBansach data = new QLBansach();
+
+        public ActionResult Index()
+        {
+            var book = from bk in data.SACHes select bk;
+            return View(book);
+        }
+        public ActionResult ChuDe()
+        {
+            var chude = from cd in data.CHUDEs select cd;
+            return PartialView(chude);
+        }
+        public ActionResult NhaXuatBan()
+        {
+            var nhaxb = from nxb in data.NHAXUATBANs select nxb;
+            return PartialView(nhaxb);
+        }
+        public ActionResult SPTheochude(int id)
+        {
+            var sach = from s in data.SACHes where s.MaCD == id select s;
+            return View(sach);
+        }
+        public ActionResult SPTheoNXB(int id)
+        {
+            var sach = from s in data.SACHes where s.MaCD == id select s;
+            return View(sach);
+        }
+        public ActionResult Details(int id)
+        {
+            var sach = from s in data.SACHes where s.Masach == id select s;
+            return View(sach.Single());
+        }
+    }
+}
